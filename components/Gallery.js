a3f668d8afda1427e9ea103b7d48af3c11b62f4d
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-08-22 18:14:59 +0530

    u

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,8 +1,8 @@
 const Gallery = () => {
   return (
-    <div class="relative mt-8 w-[95%] columns-1 gap-4 sm:columns-2 md:columns-3">
+    <div className="relative mt-8 w-[95%] columns-1 gap-4 sm:columns-2 md:columns-3">
       {new Array(14).fill(0).map((i, _) => (
-        <div key={_} class="mt-4 break-inside-avoid relative flex flex-col rounded bg-gray-200 p-[0.1rem]">
+        <div key={_} className="mt-4 break-inside-avoid relative flex flex-col rounded bg-gray-200 p-[0.1rem]">
           <img loading="lazy" src={`/gallery/${_ + 1}.jpeg`} className="w-full h-auto rounded" />
         </div>
       ))}
