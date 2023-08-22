2015dea2868937e18d21985fef50bfba0e514bbb
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-08-22 17:45:53 +0530

    u

--- components/Card.js
+++ components/Card.js
@@ -2,6 +2,8 @@ const Card = ({ name, nights, days }) => {
   return (
     <div className="w-full rounded-b bg-gray-50 flex flex-col">
       <img
+        loading="lazy"
+        height="200px"
         className="rounded-t w-full h-[200px] object-cover"
         src="https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2021&q=80"
       />
