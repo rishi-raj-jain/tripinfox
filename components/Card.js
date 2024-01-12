20bac52180c6f8b8871dcc344069e42051ecc9cd
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-01-12 12:20:22 +0530

    .

--- components/Card.js
+++ components/Card.js
@@ -10,7 +10,10 @@ const Card = ({ name, nights, days, image }) => {
       </div>
       <a
         onMouseOver={(e) => {
-          if (window.waa) window.waa.dispatchEvent('enquire-intent', { name })
+          if (window.waa) {
+            console.log('dispatch called')
+            window.waa.dispatchEvent('enquire-intent', { name })
+          }
         }}
         href="tel:+919999228200"
         className="m-4 hover:border-black mt-auto max-w-max border rounded px-2 py-1"
