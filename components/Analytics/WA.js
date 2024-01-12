598d3b264c3a7fa75ab7c4da2088a049339b9a56
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-01-12 12:04:08 +0530

    add WA

--- components/Analytics/WA.js
+++ components/Analytics/WA.js
@@ -4,7 +4,14 @@ const WA = ({ ID }) => {
   useEffect(() => {
     if (ID) {
       const script = document.createElement('script')
+      script.id = 'waa'
+      script.defer = 'defer'
       script.src = `https://stats.wideangle.co/script/${ID}.js`
+      script.onload = () => {
+        waaCreate().then((waa) => {
+          window.waa = waa
+        })
+      }
       document.head.appendChild(script)
     }
     // Clean up the script when the component unmounts
