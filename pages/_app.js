009932a44bfc6168b6f32525ffb7cfd9f4f6ca88
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-01-18 14:02:56 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import '@/styles/globals.css'
-import WA from 'components/Analytics/WA'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -8,7 +7,6 @@ export default function App({ Component, pageProps }) {
   return (
     <div className={[inter.className, 'flex flex-col w-full items-center'].join(' ')}>
       <Component {...pageProps} />
-      <WA ID="3D04G3B9ACAE3F4354" />
     </div>
   )
 }
